f360919ca13920377344793ab2bf21c33501fd3f
ShuvoProgram <shuvoprogramer@gmail.com>
2023-11-27 12:41:20 +0600

    🛠️ Added routes for courses, enrollment, and authentication middleware. 🌟 Implemented course endpoints with authentication and validation checks. 🖼️ Defined routes for course enrollment and specific course retrieval. 🚀 Created routes for courses, enrollment, and individual course retrieval. 🔒 Updated routes to include authentication and parameter validation.

--- src/app/routes/tutor/tutor.route.js
+++ src/app/routes/tutor/tutor.route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const tutorController = require('../../controller/tutor.controller')
+const tutorController = require('../../controller/tutor.controller');
+
 router
     .route('/top')
     .get(tutorController.getTopTutors)
